ddad6e5d68693e5980e7bf1cf7a67531514a32c7
AlanD20 <aland20@pm.me>
2024-03-31 16:00:19 +0200

    register policies manually

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use App\Policies\TracerPolicy;
+use App\Policies\ActivityPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Spatie\Activitylog\Models\Activity;
+use Entensy\FilamentTracer\Models\Tracer;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Tracer::class, TracerPolicy::class);
+        Gate::policy(Activity::class, ActivityPolicy::class);
+
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['en', 'ckb']);
+        });
     }
 }
